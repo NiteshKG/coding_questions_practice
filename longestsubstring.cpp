d45a4bb6f3729e10f410f6ef2cff9ef99a42701d
NiteshKG <rachnakgupta81@gmail.com>
2023-10-19 14:03:58 +0530

    mergesort

--- longestsubstring.cpp
+++ longestsubstring.cpp
@@ -8,7 +8,7 @@ int main(){
 
     vector<int> dict(256,-1);
     int maxLen=0,start = -1;
-    for(int i=0;i<s.size();i++){
+    for(int i=0;i<s.size();i++){  
         if(dict[s[i]] > start)
         start = dict[s[i]];
         dict[s[i]] = i;
